30ee054f79f6a8148b0edee07fc659a8120fdf2a
aaronomnis1 <87872660+aaronomnis1@users.noreply.github.com>
2021-10-25 11:48:44 -0400

    Update New Computer Install Downloader.ps1

--- New Computer Install Downloader.ps1
+++ New Computer Install Downloader.ps1
@@ -1,19 +1,17 @@
 ﻿
 # Modified from https://gist.github.com/Splaxi/fe168eaa91eb8fb8d62eba21736dc88a
 
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
-  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+If (!( [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )) {
+  Start-Process powershell.exe "-File",( '"{0}"' -f $MyInvocation.MyCommand.Path ) -Verb RunAs
   Exit
 }
 
 $repo = "Hor318/New-Computer-Install"
-$owner = $repo -split '/' | Select -First 1
-$repoName = $repo -split '/' | Select -Last 1
+#$owner = $repo -split '/' | Select -First 1
+#$repoName = $repo -split '/' | Select -Last 1
 
 $filenamePattern = "*New.Computer.Install.exe*"
-$pathExtract = "C:\temp\temptest"
 
-$innerDirectory = $true
 $preRelease = $false
 
 $version = Invoke-WebRequest -Uri "https://api.github.com/repos/Hor318/New-Computer-Install/releases/latest" -UseBasicParsing
@@ -39,3 +37,19 @@ If ( Test-Path "$pathNoVer*.exe" ) {
   Remove-Item "$pathNoVer*.exe" -Force 
 }
   Invoke-WebRequest -Uri $downloadUri -Out $pathFile #$pathZip
+
+  # Start .exe and run prerequisites if choco is not detected
+  $TARGETDIR = 'C:\ProgramData\Chocolatey\choco.exe'
+  If (!( Test-Path -Path $TARGETDIR )) {
+    Start-Process $pathFile -Wait -Verb Runas
+        Exit
+  }
+  
+  # Remove prerequisite log
+  $winTemp = [System.Environment]::GetEnvironmentVariable('TEMP','Machine')
+  $fileContent = Get-ChildItem $winTemp | Where-Object {$_.Name -like "New Computer Install*"} | Sort-Object LastWriteTime -Descending | Select-Object -First 1 
+    If ($fileContent.Length -lt '5000') {Remove-Item $fileContent.FullName -Force}
+
+  Start-Process $pathFile -Wait -Verb Runas
+
+Remove-Item $pathFile -Force
