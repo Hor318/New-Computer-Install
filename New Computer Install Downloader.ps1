3edc9b54c6413c523b6ed9c5c67f16649e69934b
aaronomnis1 <87872660+aaronomnis1@users.noreply.github.com>
2022-03-23 15:35:48 -0400

    Mass code formatting update
    Fixed indentation globally

--- New Computer Install Downloader.ps1
+++ New Computer Install Downloader.ps1
@@ -2,9 +2,11 @@
 # Modified from https://gist.github.com/Splaxi/fe168eaa91eb8fb8d62eba21736dc88a
 
 If (!( [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )) {
-  Start-Process powershell.exe "-File",( '"{0}"' -f $MyInvocation.MyCommand.Path ) -Verb RunAs
-  Exit
+    Start-Process powershell.exe "-File",( '"{0}"' -f $MyInvocation.MyCommand.Path ) -Verb RunAs
+    Exit
 }
+
+Set-ExecutionPolicy RemoteSigned -force
 
 $repo = "Hor318/New-Computer-Install"
 #$owner = $repo -Split '/' | Select -First 1
@@ -29,26 +31,26 @@ $pathNoVer = "$env:Public\Desktop\New.Computer.Install"
 $pathFile = "$env:Public\Desktop\New.Computer.Install $version.exe"
 
 If ( Test-Path "$pathNoVer*.exe" ) { 
-  ForEach ( $p in $( Get-ChildItem "$pathNoVer*.exe" )) {
-    Write-Warning "File [$( $p.FullName )] found. Removing and redownloading" 
-  }
+    ForEach ( $p in $( Get-ChildItem "$pathNoVer*.exe" )) {
+        Write-Warning "File [$( $p.FullName )] found. Removing and redownloading" 
+    }
 
-  Remove-Item "$pathNoVer*.exe" -Force 
+    Remove-Item "$pathNoVer*.exe" -Force 
 }
 
-  Invoke-WebRequest -Uri $downloadUri -Out $pathFile #$pathZip
+    Invoke-WebRequest -Uri $downloadUri -Out $pathFile #$pathZip
 
-  # Start .exe and run prerequisites if choco is not detected
-  $TARGETDIR = 'C:\ProgramData\Chocolatey\choco.exe'
-  If (!( Test-Path -Path $TARGETDIR )) {
-    Start-Process $pathFile -Wait -Verb Runas
-  }
+    # Start .exe and run prerequisites if choco is not detected
+    $TARGETDIR = 'C:\ProgramData\Chocolatey\choco.exe'
+        If (!( Test-Path -Path $TARGETDIR )) {
+            Start-Process $pathFile -Wait -Verb Runas
+        }
   
-  # Remove prerequisite log
-  $winTemp = [System.Environment]::GetEnvironmentVariable('TEMP','Machine')
-  $fileContent = Get-ChildItem $winTemp | Where-Object { $_.Name -like "New Computer Install*" } | Sort-Object LastWriteTime -Descending | Select-Object -First 1 
-    If ( $fileContent.Length -lt '5000' ) { Remove-Item $fileContent.FullName -Force }
+    # Remove prerequisite log
+    $winTemp = [System.Environment]::GetEnvironmentVariable('TEMP','Machine')
+    $fileContent = Get-ChildItem $winTemp | Where-Object { $_.Name -like "New Computer Install*" } | Sort-Object LastWriteTime -Descending | Select-Object -First 1 
+        If ( $fileContent.Length -lt '5000' ) { Remove-Item $fileContent.FullName -Force }
 
-  Start-Process $pathFile -Wait -Verb Runas
+    Start-Process $pathFile -Wait -Verb Runas
 
 Remove-Item $pathFile -Force
