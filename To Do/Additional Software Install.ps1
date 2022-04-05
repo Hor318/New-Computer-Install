feffedbecf9706e6b8d0858269034571e7459b7c
aaronomnis1 <87872660+aaronomnis1@users.noreply.github.com>
2022-04-05 14:23:00 -0400

    Updated Chrome install

--- To Do/Additional Software Install.ps1
+++ To Do/Additional Software Install.ps1
@@ -1,6 +1,20 @@
 
 <#
-    TO DO:
-    Manual install of Google Chrome and 7-zip due to chocolately failing checksums.. a lot.
+snippet from https://www.snel.com/support/install-chrome-in-windows-server/
 #>
 
+$LocalTempDir = $env:TEMP
+$ChromeInstaller = "ChromeInstaller.exe"
+
+(New-Object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller")
+    & "$LocalTempDir\$ChromeInstaller" /silent /install
+
+$Process2Monitor =  "ChromeInstaller"
+    Do { 
+        $ProcessesFound = Get-Process | Where-Object { $Process2Monitor -contains $_.Name } | Select-Object -ExpandProperty Name
+        If ( $ProcessesFound ) { 
+            "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 
+        } Else { 
+            Remove-Item "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose 
+        } 
+    } Until ( !$ProcessesFound ) 
